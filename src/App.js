62f7eddb5c62ae239fe30964d699521824bfcb67
JuanJacoboViera <juanjacoboviera@gmail.com>
2023-02-05 21:50:51 -0500

    add employee banner main info and styles in ProfilePage component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import {useState, useEffect} from 'react'
 import Dashboard from './components/Dashboard';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ProfilePage from './components/ProfilePage';
-import { useParams } from 'react-router-dom'
 
 
 function App() {
-  const {profileId} = useParams()
-  const [id, setId] = useState('');
-  const [singleEmployee, setSingleEmployee] = useState({});
   const [employees, setemployees] = useState([]);
   const [sortByAge, setSortByAge] = useState('oldest');
   const seed ='e4d249cc94fc478b';
@@ -25,17 +21,13 @@ const sortByDOB = (people) => {
     });
   }
 
-  // const getId = (identification) =>{
-  //   setId(identification)
-  // }
-
 useEffect(()=>{
   fetch(workForceUrl)
   .then(response => response.json())
   .then(data => setemployees(data.results))
 
 
-},[sortByAge, profileId])
+},[sortByAge])
 
   return (
     <BrowserRouter>
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import {useState, useEffect} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
+import { faLocationPin } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
 const ProfilePage = ({employees}) => {
       const [employee, setemployee] = useState()
       const {profileId} = useParams()
       
       const findEmployee = (users) =>{
-        const firstWord = profileId.split("-")[0]
+        const firstWord = profileId.split("-")[1]
         const user = users.filter(employee => {
-          return employee.name.first == firstWord
+          return employee.name.last == firstWord
         })
         return user
       }
@@ -21,8 +26,27 @@ const ProfilePage = ({employees}) => {
      console.log(employee)
   
   return (
-    <div>
-      {employee && <h1>{employee.name.first} {employee.name.last}</h1>}
+    <div className='container'>
+      <header className='profile__banner'>
+      <div className='profile__banner-background bg-pan-left'>
+        {employee && <img className="employee__img" src={employee.picture.large} alt="Employee" width="100px" height="100px"/>}
+        <div className='profile__banner__info'>
+        {employee && <h1>{employee.name.first} {employee.name.last}</h1>}
+        {employee && <p> Age: {employee.dob.age}</p>}
+        </div>
+      </div>
+      <div className='profile__banner__secondaryInfo'>
+        <FontAwesomeIcon icon={faPhone}/>
+        {employee && <p> {employee.cell}</p>}
+        <FontAwesomeIcon icon={faLocationPin}/>
+        {employee && <p>{employee.location.country}</p>}
+        <FontAwesomeIcon icon={faEnvelope}/>
+       {employee && <p> {employee.email} </p>}
+
+
+      </div>
+
+      </header>
     </div>
   )
 }
