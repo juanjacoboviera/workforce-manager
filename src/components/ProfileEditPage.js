72e2fa6a4513564b392c139ba4aaaa0827a17870
JuanJacoboViera <juanjacoboviera@gmail.com>
2023-02-14 11:02:39 -0500

    add html structure to ProfileEditPage component

--- src/components/ProfileEditPage.js
+++ src/components/ProfileEditPage.js
@@ -2,7 +2,74 @@ import React from 'react'
 
 const ProfileEditPage = () => {
   return (
-    <div>ProfileEditPage</div>
+    <div className='editPage__container'>
+        <div className='leftCol__container'>
+            <div className='img__container'>
+                <img src="" alt="" />
+            </div>
+            <div className='text__container'>
+                <h2>Juan's <br />Profile page</h2>
+            </div>
+        </div>
+        <div className='rightCol__container'>
+            <h2>Profile Information</h2>
+            <div className="rightCol__grid">
+                <form action="submit">
+                    <div>
+                        <label htmlFor="firstName">First name</label>
+                        <input id='firstName' type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Last name</label>
+                        <input id='lastName' type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="dob"> Date of Birth</label>
+                        <input id='dob'  type="date" />
+                    </div>
+                    <div>
+                        <label htmlFor="cellPhone">Cell Phone</label>
+                        <input id='cellPhone' type="number" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">E-mail</label>
+                        <input id='email' type="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="gender">Gender</label>
+                        <input id='gender' type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="country">Country</label>
+                        <input id='country'  type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="city">City</label>
+                        <input id='city' type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="streetNumber">Street Number</label>
+                        <input id='streetNumber' type="number" />
+                    </div>
+                    <div>
+                        <label htmlFor="streetName">Street Name</label>
+                        <input id='streetName' type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="latitude">Latitude</label>
+                        <input id='latitude' type="number" />
+                    </div>
+                    <div>
+                        <label htmlFor="longitude">Longitude</label>
+                        <input id='longitude' type="number" />
+                    </div>
+                    <button type='submit'>Update</button>
+                </form>
+            </div>
+            
+        </div>
+
+    </div>
   )
 }
 
